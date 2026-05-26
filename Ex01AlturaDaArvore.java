08c5c469ec38d22bfa8688012ef60dd9e69976b4
lehnerdola <ln9458143@gmail.com>
2026-05-26 09:28:48 -0300

    corrigido

--- Ex01AlturaDaArvore.java
+++ Ex01AlturaDaArvore.java
@@ -34,6 +34,6 @@
             public static void main(String[] args) {
                 Node raiz = exemploPastas();
 
-                System.out.println("Altura da árvore: " + altura(raiz));
+                System.out.println("Altura da arvore: " + altura(raiz));
             }
         }
